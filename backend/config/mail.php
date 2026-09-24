58f96bccb63b621ab94742af1ea40fb759edab0c
Fiza Nazz <fizanaazz321@gmail.com>
2026-09-24 13:31:18 +0500

    Add explicit encryption config to mail smtp mailer

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -47,6 +47,7 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => (int) env('MAIL_TIMEOUT', 10),
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
         ],
 
         'ses' => [
